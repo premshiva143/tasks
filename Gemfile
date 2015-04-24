ff3a8db6756812a72bd588da98af4bd28e2e9b80
estebangallego <egallego00@gmail.com>
2015-04-23 23:44:04 -0700

    second_commit

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
 source 'https://rubygems.org'
+gem 'hirb'
+gem 'launchy'
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -2,9 +2,23 @@ require 'rails_helper'
 
 RSpec.describe "Tasks", type: :request do
   describe "GET /tasks" do
-  	it "display some task"
-  		visit task_path
+  	it "display some task" do
+  		@task = Tasks.create :task => 'go to bed'
+
+  		visit tasks_path
   		page.should have_content 'go to bed'
 
     end
+
+    it "creates a new task" do
+    	visit tasks_path
+    	fill_in 'Task', :with => 'go to work'
+    	click_button 'Create Task'
+
+    	current_path.should == root_path
+    	page.should have_content 'go to work'
+
+    	save_and_open_page
+    end
   end
+end
