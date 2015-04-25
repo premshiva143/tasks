fa6fa54e60717f805e040f0eb767dc0e3ef4d434
Rajarshi Das <rajarshi2576@gmail.com>
2015-04-25 12:06:14 +0530

    fix visit error for spec

--- spec/features/task_spec.rb
+++ spec/features/task_spec.rb
@@ -1,24 +1,24 @@
-# require "rails_helper"
+require "rails_helper"
 
-# RSpec.describe "Tasks", type: :request do
-#   config.include Capybara::DSL
-#   describe "GET /tasks" do
-#   	it "display some task" do
-#   		@task = Task.create :task => "go to bed"
-#   		visit tasks_path
-#   		page.should have_content "go to bed"
+RSpec.describe "Tasks", type: :request do
+  config.include Capybara::DSL
+  describe "GET /tasks" do
+  	it "display some task" do
+  		@task = Task.create :task => "go to bed"
+  		visit tasks_path
+  		page.should have_content "go to bed"
 
-#     end
+    end
 
-#     it "creates a new task" do
-#     	visit "/"
-#     	fill_in "Task", :with => "go to work"
-#     	click_button "Create Task"
+    it "creates a new task" do
+    	visit "/"
+    	fill_in "Task", :with => "go to work"
+    	click_button "Create Task"
 
-#     	current_path.should == root_path
-#     	page.should have_content "go to work"
+    	current_path.should == root_path
+    	page.should have_content "go to work"
     	
-#     	save_and_open_page
-#     end
-#   end
-# end
+    	save_and_open_page
+    end
+  end
+end
