4d68d96959db8ecfdded5f4960a81ad3299f44de
estebangallego <egallego00@gmail.com>
2015-04-24 23:16:09 -0700

    troubleshooting_rspec

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,14 @@ class TasksController < ApplicationController
   def index
   	@tasks = Task.all
   end
+  def create
+  	# render :text => params.inspect
+  	Task.create(task_params)
+  	redirect_to request.referer
+  end
+
+  private
+  def task_params    
+    params.require(:task).permit(:task)
+  end
 end
--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -1,23 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe "Tasks", type: :request do
+  config.include Capybara::DSL
   describe "GET /tasks" do
   	it "display some task" do
-  		@task = Tasks.create :task => 'go to bed'
-
-  		visit tasks_path
+  		@task = Task.create :task => 'go to bed'
+  		visit root_path
   		page.should have_content 'go to bed'
 
     end
 
     it "creates a new task" do
-    	visit tasks_path
+    	visit '/'
     	fill_in 'Task', :with => 'go to work'
     	click_button 'Create Task'
 
-    	current_path.should == root_path
+    	current_path.should == task_path
     	page.should have_content 'go to work'
-
+    	
     	save_and_open_page
     end
   end
